980c1b7be9ed90cdd4b7ad954dbef09edbc75302
Arno Visker <arno@bigbridge.nl>
2024-02-27 15:53:50 +0100

    added new utility class FileStorage

---
+++ docs/FileStorage.md
@@ -0,0 +1,93 @@
+# File Storage Documentation
+
+The `FileStorage` class offers a simple and efficient way to handle file storage operations within a filesystem. This class is designed to be intuitive, providing functionalities such as creating directories, storing, retrieving, and deleting files.
+
+## All methods available in the FileStorage class
+
+- [constructor()](#constructor)  Constructs a new FileStorage instance.
+- [dir()](#dir) Create or set the directory context for file operations.
+- [put()](#put) Stores content in a file at the specified path.
+- [delete()](#delete) Deletes a file from the specified path.
+- [allFiles()](#allFiles) Retrieves all files in the current or a specified directory.
+- [lastModified()](#lastModified) Gets the last modification time of a specified file.
+
+### Constructing a FileStorage Instance
+
+To start using the `FileStorage`, instantiate it with the path of the directory where files should be stored.
+
+```php
+$storage = new FileStorage('/path/to/storage');
+```
+### dir()
+
+Create or set the directory context for file operations. If the directory does not exist, it will be created.
+
+```php
+// Return a new instance of FileStorage with the new set path.
+$storage->dir('user/images');
+```
+
+### put()
+
+Stores content in a file at the specified path. If the file does not exist, it will be created.
+
+```php
+// Store 'Hello, world!' in 'greetings.txt' 
+$storage->put('greetings.txt', 'Hello, world!');
+```
+
+Store files in sub directory of the FileStorage
+```php
+// Store greetings in a sub directory
+$storage->dir('someDir/anotherDir')->put('greetings.txt', 'Hello, world!');
+// Or shorthand without setting the dir context
+$storage->put('someDir/anotherDir/greetings.txt', 'Hello, world!');
+```
+### delete()
+
+Deletes a file from the specified path.
+
+```php
+// Delete 'oldfile.txt' from storage 
+$storage->delete('oldfile.txt');
+```
+
+Delete a file located in a sub directory
+```php
+// Delete a file inside a nested directory
+$storage->dir('someDir/anotherDir')->delete('oldfile.txt');
+// Or shorthand without setting the dir context
+$storage->delete('someDir/anotherDir/oldfile.txt');
+```
+### allFiles()
+
+Retrieves the path of all files in the current or a specified directory.
+
+```php
+// Get all files in the storage root
+$files = $storage->allFiles();
+
+// Get all files in the 'documents' directory
+$filesInDocuments = $storage->allFiles('documents');
+
+// Get all files in a subdirectory using dir()
+$filesInSubDir = $storage->dir('someDir/anotherDir')->allFiles();
+
+// Alternatively, without setting the dir context
+$filesInSubDirDirect = $storage->allFiles('someDir/anotherDir');
+```
+
+### lastModified()
+
+Gets the last modification time of a specified file.
+
+```php
+// Get the last modification time of 'report.txt'
+$lastModified = $storage->lastModified('report.txt');
+
+// Get the last modification time of a file in a subdirectory
+$lastModifiedInSubDir = $storage->lastModified('someDir/anotherDir/report.txt');
+
+// Using dir() to set the context before getting the last modification time
+$lastModifiedUsingDir = $storage->dir('someDir/anotherDir')->lastModified('report.txt');
+```
---
+++ src/FileStorage.php
@@ -0,0 +1,156 @@
+<?php
+declare(strict_types=1);
+
+namespace Avmg\PhpSimpleUtilities;
+
+use InvalidArgumentException;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use SplFileInfo;
+
+class FileStorage
+{
+    protected string $basePath;
+
+    /**
+     * Constructor.
+     *
+     * @param string $path Path to store files.
+     */
+    public function __construct(string $path) {
+        // Check if the path is a directory and exists
+        if (!is_dir($path)) {
+            throw new InvalidArgumentException('The path is not a directory or does not exist.');
+        }
+
+        // Check if the path is writable
+        if (!is_writable($path)) {
+            throw new InvalidArgumentException('The path is not writable.');
+        }
+
+        // Set the base path
+        $this->basePath = realpath(rtrim($path, DIRECTORY_SEPARATOR)) . DIRECTORY_SEPARATOR;
+    }
+
+    /**
+     * Create directories recursively based on the given path.
+     *
+     * @param string $path Path or Name for the directory. 'avatars' or 'user/profiles/avatars' for example.
+     * @return static New instance of FileStorage with the selected path.
+     */
+    public function dir(string $path): static
+    {
+        $newPath = $this->basePath . trim($path, DIRECTORY_SEPARATOR);
+
+        if (!is_dir($newPath)) {
+            if (!mkdir($newPath, 0755, true)) {
+                throw new InvalidArgumentException('The directory could not be created.');
+            }
+        }
+
+        return new self($newPath);
+    }
+
+    /**
+     * Put a file in the selected path.
+     *
+     * @param string $filename Name or Path of the file starting from the base path.
+     * @param mixed $content Content to be written.
+     */
+    public function put(string $filename, mixed $content): void {
+        $fullPath = $this->getFullPath($filename);
+
+        if (false === file_put_contents($fullPath, $content)) {
+            throw new InvalidArgumentException('The file could not be written.');
+        }
+    }
+
+    /**
+     * Delete a file from the selected path.
+     *
+     * @param string $filename Name or Path of the file starting from the base path.
+     */
+    public function delete(string $filename): void {
+        $fullPath = $this->getFullPath($filename);
+
+        if (file_exists($fullPath)) {
+            if (!unlink($fullPath)) {
+                throw new InvalidArgumentException('The file could not be deleted.');
+            }
+        }
+    }
+
+    /**
+     * Retrieve all files in the current basePath. $path is optional, but if $path is used, dir will be called to retrieve an instance of that path.
+     *
+     * @param string|null $path Optional path to list files from 'avatars' or 'user/profiles/avatars'.
+     * @return string[] An array of all file paths in the given directory
+     */
+    public function allFiles(?string $path = null): array {
+        $directory = $this->basePath;
+
+        if ($path !== null) {
+            $fileStorage = $this->dir($path);
+            $directory = $fileStorage->getBasePath();
+        }
+
+        $files = [];
+        $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directory));
+
+        /** @var SplFileInfo $file */
+        foreach ($iterator as $file) {
+            if ($file->isFile()) {
+                $files[] = $file->getRealPath();
+            }
+        }
+
+        return $files;
+    }
+
+    /**
+     * The lastModified method returns the UNIX timestamp of the last time the file was modified.
+     *
+     * @param string $filename Name or Path of the file starting from the base path.
+     * @return int The UNIX timestamp of the last modification time.
+     */
+    public function lastModified(string $filename): int {
+        $fullPath = $this->getFullPath($filename);
+
+        if (!file_exists($fullPath)) {
+            throw new InvalidArgumentException('The file does not exist.');
+        }
+
+        return filemtime($fullPath);
+    }
+
+    /**
+     * @return string The base path of this instance
+     */
+    public function getBasePath(): string
+    {
+        return $this->basePath;
+    }
+
+    /**
+     * Get the full path of the file creating directories if needed.
+     *
+     * @param string $filename Name or Path of the file starting from the base path.
+     * @return string The full path of the file
+     */
+    protected function getFullPath(string $filename): string
+    {
+        $directory = dirname($filename);
+
+        // check if a path is provided
+        if ($directory !== '.') {
+            $instance = $this->dir($directory); // Ensure directory exists
+            // Get the full path using newly created instance which has the correct path
+            $fullPath = $instance->getBasePath() . basename($filename);
+        } else {
+            // Get the full path using the current instance
+            $fullPath = $this->basePath . $filename;
+        }
+
+        return $fullPath;
+    }
+}
