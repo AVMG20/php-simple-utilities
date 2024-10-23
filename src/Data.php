48389d7705f71e5ee2eadc149da3fb7d2e23b914
Arno Visker <arno@bigbridge.nl>
2024-10-23 09:06:34 +0200

    updated data object class

--- src/Data.php
+++ src/Data.php
@@ -8,6 +8,7 @@ use InvalidArgumentException;
 use JsonSerializable;
 use ReflectionClass;
 use ReflectionException;
+use ReflectionNamedType;
 use ReflectionParameter;
 use UnitEnum;
 
@@ -15,7 +16,8 @@ use UnitEnum;
  * Base DTO Data class
  *
  * This class provides a base implementation for Data Transfer Objects (DTOs).
- * It includes a static factory method for creating new instances from an array of attributes,
+ * It includes functionality for creating instances from arrays, type casting,
+ * and converting objects back to arrays.
  */
 abstract class Data implements JsonSerializable
 {
@@ -30,189 +32,192 @@ abstract class Data implements JsonSerializable
     {
         $reflectionClass = new ReflectionClass(static::class);
         $constructor = $reflectionClass->getConstructor();
-        $args = [];
 
-        if ($constructor) {
-            // Loop through all parameters of the constructor
-            foreach ($constructor->getParameters() as $parameter) {
-
-                // Get the value for each parameter from the attributes array
-                $value = self::getValue($parameter, $attributes);
-
-                // Determine the value based on the parameter type
-                $value = self::castValue($parameter, $value);
-
-                $args[] = $value;
-            }
+        if (!$constructor) {
+            return new static();
         }
 
-        return $reflectionClass->newInstanceArgs($args);
+        return $reflectionClass->newInstanceArgs(
+            self::resolveConstructorParameters($constructor->getParameters(), $attributes)
+        );
     }
 
     /**
-     * Get the value for each parameter from the attributes array.
+     * Resolve constructor parameters from attributes array.
      *
-     * @param ReflectionParameter $parameter The parameter to get the value for.
-     * @param array $attributes The source attributes for creating the data object.
-     * @return mixed Returns the value for the parameter.
-     * @throws ReflectionException Throws an exception if the parameter is required but not found in the attributes array.
-     */
-    protected static function getValue(ReflectionParameter $parameter, array $attributes): mixed
-    {
-        $name = $parameter->getName();
-        $type = $parameter->getType();
-
-        $isNullable = $type && $type->allowsNull();
-        $hasDefaultValue = $parameter->isDefaultValueAvailable();
-        $isOptional = $hasDefaultValue || $isNullable;
-
-        // Check if the parameter is included in the provided attributes
-        if (!array_key_exists($name, $attributes)) {
-
-            // Check if the parameter that was not found in the provided attributes is optional
-            if ($isOptional) {
-                // Return default value or null if parameter is optional
-                return $isNullable && !$hasDefaultValue ? null : $parameter->getDefaultValue();
-            }
-
-            // Throw an exception if the parameter is required but not found in the attributes array
-            throw new InvalidArgumentException("Missing required attribute: '{$name}' in " . static::class . "::from() method.");
-        }
-
-        // Return the value from the attributes array
-        return $attributes[$name];
-    }
-
-    /**
-     * Cast the value to the requested type.
-     * We let the reflection system handle casting built-in types.
-     *
-     * @param ReflectionParameter $parameter The parameter to cast the value for.
-     * @param mixed $value The value to cast.
-     * @return mixed|static Returns the cast value.
+     * @param array<ReflectionParameter> $parameters
+     * @param array $attributes
+     * @return array
      * @throws ReflectionException
      */
-    protected static function castValue(ReflectionParameter $parameter, $value)
+    private static function resolveConstructorParameters(array $parameters, array $attributes): array
+    {
+        return array_map(fn(ReflectionParameter $parameter) => self::castValue(
+            $parameter,
+            self::resolveParameterValue($parameter, $attributes)
+        ),
+            $parameters
+        );
+    }
+
+    /**
+     * Resolve the value for a parameter from attributes array.
+     *
+     * @param ReflectionParameter $parameter
+     * @param array $attributes
+     * @return mixed
+     * @throws ReflectionException
+     */
+    private static function resolveParameterValue(ReflectionParameter $parameter, array $attributes): mixed
+    {
+        $name = $parameter->getName();
+
+        if (array_key_exists($name, $attributes)) {
+            return $attributes[$name];
+        }
+
+        if ($parameter->isDefaultValueAvailable()) {
+            return $parameter->getDefaultValue();
+        }
+
+        if ($parameter->getType()?->allowsNull()) {
+            return null;
+        }
+
+        throw new InvalidArgumentException(
+            sprintf("Missing required attribute: '%s' in %s::from() method.", $name, static::class)
+        );
+    }
+
+    /**
+     * Cast a value to the requested type.
+     *
+     * @param ReflectionParameter $parameter
+     * @param mixed $value
+     * @return mixed
+     * @throws ReflectionException
+     */
+    private static function castValue(ReflectionParameter $parameter, mixed $value): mixed
     {
         $type = $parameter->getType();
 
-        if ($type && !$type->isBuiltin() && $value !== null) {
-            $typeName = $type->getName();
-
-            // Cast to enum
-            if (enum_exists($typeName)) {
-                // Bypass casting if value is already of the correct enum type
-                /** @var UnitEnum|BackedEnum $typeName */
-                if ($value instanceof $typeName) {
-                    return $value;
-                }
-                // Attempt to cast to enum
-                if ($enum = $typeName::tryFrom($value)) {
-                    return $enum;
-                } else {
-                    throw new InvalidArgumentException("Invalid enum value '{$value}' for enum type '{$typeName}'.");
-                }
-            }
-
-            // Cast to Data object
-            if (is_subclass_of($typeName, self::class)) {
-                /** @var static $typeName */ // check if value is not already of type data object.
-                // if the user has already initialized the data object, we just return it
-                // else create a new data object from the array, allowing the user to choose how to nest data objects
-                if ($value instanceof $typeName) {
-                    // return already initialized data object
-                    return $value;
-                }
-
-                if (is_array($value)) {
-                    // initialize data object from array
-                    return $typeName::from($value);
-                }
-
-                // Throw InvalidArgumentException if value is not an array or Data object
-                if (is_object($value)) {
-                    $type = get_class($value);
-                    throw new InvalidArgumentException("Invalid value of type '{$type}' for type '{$typeName}', expected array. in " . static::class . "::from() method.");
-                } else {
-                    throw new InvalidArgumentException("Invalid value '{$value}' for type '{$typeName}', expected array. in " . static::class . "::from() method.");
-                }
-            }
-
-            // Just return value if no cast is found
+        if (!$type instanceof ReflectionNamedType || $type->isBuiltin() || $value === null) {
             return $value;
         }
 
-        // return value if type is built-in or value is null
+        $typeName = $type->getName();
+
+        if (enum_exists($typeName)) {
+            return self::castToEnum($value, $typeName);
+        }
+
+        if (is_subclass_of($typeName, self::class)) {
+            return self::castToDataObject($value, $typeName);
+        }
+
         return $value;
     }
 
+    /**
+     * Cast a value to an enum type.
+     *
+     * @param mixed $value
+     * @param class-string<UnitEnum|BackedEnum> $enumClass
+     * @return UnitEnum|BackedEnum
+     * @throws InvalidArgumentException
+     */
+    private static function castToEnum(mixed $value, string $enumClass): UnitEnum|BackedEnum
+    {
+        if ($value instanceof $enumClass) {
+            return $value;
+        }
+
+        if ($enum = $enumClass::tryFrom($value)) {
+            return $enum;
+        }
+
+        throw new InvalidArgumentException(
+            sprintf("Invalid enum value '%s' for enum type '%s'.", $value, $enumClass)
+        );
+    }
 
     /**
-     * Convert the object to an array, including all nested Data objects.
+     * Cast a value to a Data object type.
      *
-     * @return array The object converted to an array.
+     * @param mixed $value
+     * @param class-string<self> $dataClass
+     * @return self
+     * @throws InvalidArgumentException
      */
-    public function toArray(): array {
+    private static function castToDataObject(mixed $value, string $dataClass): self
+    {
+        if ($value instanceof $dataClass) {
+            /** @var self $value */
+            return $value;
+        }
+
+        if (is_array($value)) {
+            return $dataClass::from($value);
+        }
+
+        throw new InvalidArgumentException(
+            sprintf(
+                "Invalid value of type '%s' for type '%s', expected array in %s::from() method.",
+                is_object($value) ? get_class($value) : gettype($value),
+                $dataClass,
+                static::class
+            )
+        );
+    }
+
+    /**
+     * Convert the object to an array.
+     *
+     * @return array
+     */
+    public function toArray(): array
+    {
         $result = [];
 
-        // Loop through all properties of the object
         foreach ($this as $propertyName => $value) {
-
-            // Convert UnitEnum to string
-            if ($value instanceof UnitEnum) {
-                $result[$propertyName] = $value instanceof BackedEnum ? $value->value : $value->name;
-                continue; // value converted, skip to next property
-            }
-
-            // Recursively convert nested Data objects or iterables to arrays
             if (is_iterable($value)) {
-
-                // Convert all items in the array
-                $result[$propertyName] = array_map(function ($item) {
-
-                    // Convert UnitEnum to string
-                    if ($item instanceof UnitEnum) {
-                        return $item instanceof BackedEnum ? $item->value : $item->name;
-                    }
-
-                    // Recursively convert nested Data objects to arrays
-                    if ($item instanceof self) {
-                        return $item->toArray();
-                    }
-
-                    // Convert Data objects in arrays to arrays
-                    if (is_object($item) && method_exists($item, 'toArray')) {
-                        return $item->toArray();
-                    }
-
-                    // Just return the value if no conversion method is found
-                    return $item;
-                }, is_array($value) ? $value : iterator_to_array($value));
-
-                continue; // value converted, skip to next property
+                $result[$propertyName] = array_map(
+                    fn($item) => $this->itemToArray($item),
+                    is_array($value) ? $value : iterator_to_array($value)
+                );
+                continue;
             }
 
-            // Convert object to array if it has a toArray method
-            if (is_object($value) && method_exists($value, 'toArray')) {
-                $result[$propertyName] = $value->toArray();
-                continue; // value converted, skip to next property
-            }
-
-            // Just set the value if no conversion method is found
-            $result[$propertyName] = $value;
+            $result[$propertyName] = $this->itemToArray($value);
         }
 
         return $result;
     }
 
     /**
-     * Specify data which should be serialized to JSON.
-     * Utilizes the toArray method for JSON conversion.
-     *
-     * @return mixed Data which can be serialized by json_encode, which is a value of any type other than a resource.
+     * @return mixed
      */
     public function jsonSerialize(): mixed
     {
         return $this->toArray();
+    }
+
+    /**
+     * Convert an item to its array representation.
+     *
+     * @param mixed $item
+     * @return mixed
+     */
+    private function itemToArray(mixed $item): mixed
+    {
+        if ($item instanceof UnitEnum) {
+            return $item instanceof BackedEnum ? $item->value : $item->name;
+        }
+
+        if ($item instanceof self || (is_object($item) && method_exists($item, 'toArray'))) {
+            return $item->toArray();
+        }
+
+        return $item;
     }
 }
